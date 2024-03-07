11ff375529a52cb35e415e2f6fb6a82630cb36a4
Andrea <ren@Andreas-MacBook-Air.local>
2024-03-07 09:58:51 -0600

    Module-02 Assigment

---
+++ Assignments/Module-02/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+// app.use((req,res,next) => {
+//   console.log('Middleware 1');
+//   next();
+// });
+
+// app.use((req,res,next) => {
+//   console.log('Middleware 2');
+// });
+
+app.use('/users', (req, res, next) => {
+  res.send('<h1>Users Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Home Page</h1>');
+});
+
+app.listen(3000);
--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const http = require('http'); //import global module
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
+//  ** MIDDLEWARE ZONE **
+app.use((req, res, next) => {
+  console.log('1. Inside Middleware'); // Always runs
+  next(); // Allows request to continue to the next middleware
+});
 
-const server = http.createServer(routes);
+app.use('/products', (req, res, next) => {
+  res.send('<h1>Hello From Products!</h1>');
+});
 
-server.listen(3000);
+app.use('/', (req, res, next) => {
+  console.log('2. Inside Middleware');
+  res.send('<h1>Hello From Express!</h1>');
+});
+
+//  ** MIDDLEWARE ZONE **
+
+app.listen(3000);
